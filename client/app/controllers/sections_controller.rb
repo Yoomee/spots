93125a05f56faee3d7f01ec0950e4fa49bebd4b4
Rich Wells <rich@yoomee.com>
2011-07-13 11:20:46 +0100

    updated stuff

---
+++ client/app/controllers/sections_controller.rb
@@ -0,0 +1,14 @@
+SectionsController.class_eval do
+
+  def show_with_client
+    if @section.view == "normal"
+      @pages_sections = @section.pages + @section.children
+      @pages_sections = @pages_sections.sort_by(&:weight)
+      render
+    else
+      show_without_client
+    end
+  end
+  alias_method_chain :show, :client
+
+end
