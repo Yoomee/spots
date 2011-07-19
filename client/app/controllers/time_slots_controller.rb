054c12da16dd233e250d491cb7ea0088903da819
Matt Atkins <matt@yoomee.com>
2011-07-19 11:35:59 +0100

    Delete link

--- client/app/controllers/time_slots_controller.rb
+++ client/app/controllers/time_slots_controller.rb
@@ -22,13 +22,19 @@ class TimeSlotsController < ApplicationController
         page["activity_#{activity.id}_time_slots"].append(render("time_slots/time_slot", :time_slot => @time_slot))
         @time_slot = activity.time_slots.build(:organisation => @time_slot.organisation)
       end
-      page["activity_#{activity.id}_time_slot_form"].replace(render("time_slots/form", :time_slot => @time_slot))
+      page["activity_#{activity.id}_time_slot_form"].replace(render("time_slots/form", :time_slot => @time_slot, :disabled => false))
       page << "#{labelify_javascript(:script_tag => false)};TimeSlot.setAllSelected();"
     end
   end
   
   def destroy
-    
+    @time_slot = TimeSlot.find(params[:id])
+    unique_id = "activity_#{@time_slot.activity_id}_time_slot_#{@time_slot.id}"
+    render :update do |page|
+      if @time_slot.destroy
+        page[unique_id].remove
+      end
+    end
   end
   
   def show
@@ -43,7 +49,7 @@ class TimeSlotsController < ApplicationController
       if @time_slot.update_attributes(params[:time_slot])
         page["activity_#{activity.id}_time_slot_#{@time_slot.id}"].replace(render("time_slots/time_slot", :time_slot => @time_slot))
       else
-        page["activity_#{activity.id}_time_slot_form_#{@time_slot.id}"].replace(render("time_slots/form", :time_slot => @time_slot))
+        page["activity_#{activity.id}_time_slot_form_#{@time_slot.id}"].replace(render("time_slots/form", :time_slot => @time_slot, :disabled => false))
       end
       page << "#{labelify_javascript(:script_tag => false)};TimeSlot.setAllSelected();"
     end
