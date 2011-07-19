bef13d1f0f953734ad565f234362d9060da3c8f9
Matt Atkins <matt@yoomee.com>
2011-07-19 11:18:55 +0100

    Timeslots form.

--- client/app/controllers/time_slots_controller.rb
+++ client/app/controllers/time_slots_controller.rb
@@ -7,6 +7,11 @@ class TimeSlotsController < ApplicationController
     organisation.owned_by?(member) || (member && member.is_admin?)
   end
   
+  custom_permission [:update, :destroy] do |url_options, member|
+    time_slot = TimeSlot.find(url_options[:id])
+    time_slot.organisation.owned_by?(member) || (member && member.is_admin?)
+  end
+  
   def index
     @organisation = Organisation.find(params[:organisation_id])
   end
@@ -22,6 +27,10 @@ class TimeSlotsController < ApplicationController
       page["activity_#{activity.id}_time_slot_form"].replace(render("time_slots/form", :time_slot => @time_slot))
       page << "#{labelify_javascript(:script_tag => false)};TimeSlot.setAllSelected();"
     end
+  end
+  
+  def destroy
+    
   end
   
   def show
