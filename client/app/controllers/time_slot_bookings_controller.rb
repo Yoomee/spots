4be1afeee41b21a531f078de2d6d3018f1dbf1d6
Ian Mooney <ian@yoomee.com>
2011-08-08 17:41:11 +0100

    Updated big_print

--- client/app/controllers/time_slot_bookings_controller.rb
+++ client/app/controllers/time_slot_bookings_controller.rb
@@ -10,7 +10,7 @@ class TimeSlotBookingsController < ApplicationController
   def create
     @time_slot_booking = logged_in_member.time_slot_bookings.build(params[:time_slot_booking])
     if @time_slot_booking.save
-      flash[:notice] = "Thanks #{logged_in_member.forename}, you are now booked to be at #{@time_slot_booking.organisation_name} #{@time_slot_booking.starts_at_neat_string}"
+      flash[:notice] = "Thanks #{logged_in_member.forename}, you are now booked to be at #{@time_slot_booking.organisation_name} on #{@time_slot_booking.starts_at_neat_string}"
       redirect_to thank_you_time_slot_booking_path(@time_slot_booking)
     else
       @time_slot = @time_slot_booking.time_slot
