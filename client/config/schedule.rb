738e87c5c0d3c5773eab091eda12c81215814bc6
Matt Atkins <matt@yoomee.com>
2012-01-11 16:41:24 +0000

    Fix to logging in schedule

--- client/config/schedule.rb
+++ client/config/schedule.rb
@@ -1,12 +1,14 @@
 every 1.day, :at => '3pm' do
-  set :output, File.expand_path("#{File.dirname(__FILE__)}/../../../shared/sphinx_rebuilds.log")
+  set :output, File.expand_path("#{File.dirname(__FILE__)}/../../../../shared/sphinx_rebuilds.log")
   rake "ts:index -t"
 end
 
 every 1.day, :at => '12am' do
+  set :output, File.expand_path("#{File.dirname(__FILE__)}/../../../../shared/weekly_emails.log")
   rake "spots:daily -t"
 end
 
 every 7.days, :at => '12am' do
+  set :output, File.expand_path("#{File.dirname(__FILE__)}/../../../../shared/sphinx_rebuilds.log")
   rake "spots:weekly -t"
 end
