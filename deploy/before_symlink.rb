a00851ce6365835278266604c404d912ddbb8cdc
Si Wilkins <si@yoomee.com>
2012-02-20 12:30:07 +0000

    Setup facebooker for staging

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -22,3 +22,13 @@ run "rm -rf #{release_path}/db/sphinx"
 run "mkdir -p #{shared_path}/sphinx"
 run "ln -nfs #{shared_path}/sphinx #{release_path}/db/sphinx"
 run "ln -nfs #{shared_path}/production.sphinx.conf #{release_path}/config/production.sphinx.conf"
+
+if release_path =~ /staging/
+  fb_yaml_path = "#{release_path}/client/config/facebooker.yml"
+  fb_yaml = YAML.load_file(fb_yaml_path)
+  fb_yaml['production']['app_id'] = '366926473327119'
+  fb_yaml['production']['secret'] = '1a3b7de8cab340c8cf336bd42e85d257'
+  File.open(fb_yaml_path, 'w') do |file|
+    file.puts YAML::dump(fb_yaml)
+  end
+end
